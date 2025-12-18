c94ca80c3ddfe477a8ef92afff3e1c848a5ece53
abdulsamad <gbadebosamad0@gmail.com>
2025-12-17 19:35:30 -1100

    feat: add express-session dependency, update user handlers, and define user interface in global types

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,7 +1,8 @@
-import { Request, Response } from "express";
+import { Request, Response } from "express-serve-static-core";
 import { CreateUserDto } from "../dtos/CreateUser.dto";
 import { CreateUserQueryParams } from "../types/query-params";
 import { User } from "../types/response";
+import { request } from "express";
 
 export function getAllUsers(req: Request, res: Response) {
     res.send("All users are here");
@@ -16,7 +17,8 @@ export function createUser(
     req: Request<{},{}, CreateUserDto, CreateUserQueryParams>,
     res: Response<User>)
     {
-    res.status(201).send({
+    
+    return res.status(201).send({
         id: req.body.id,
         email: req.body.email,
         username: req.body.username
